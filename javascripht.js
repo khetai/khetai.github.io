aa91bd767fe8fc6095686c5a3250af28017f3c40
khetai <78977971+khetai@users.noreply.github.com>
2021-11-11 00:29:05 +0400

    edit

--- javascripht.js
+++ javascripht.js
@@ -44,6 +44,17 @@ function funk1(x) {
     document.getElementById("trees8").src = "img/trees2ng.png";
     document.getElementById("otlar").src = "img/otlarng.png";
     document.getElementById("phon").src = "img/phone2ng.png";
+
+    document.getElementById("cloud2").src = "img/—Pngtree—white cloud hd transparent png_3595710ng.png";
+    document.getElementById("cloud3").src = "img/—Pngtree—white cloud hd transparent png_3595710ng.png";
+    document.getElementById("cloud4").src = "img/—Pngtree—white cloud hd transparent png_3595710ng.png";
+    document.getElementById("cloud1").src = "img/—Pngtree—white cloud hd transparent png_3595710ng.png";
+    document.getElementById("cloud5").src = "img/cl1ng.png";
+    document.getElementById("cloud51").src = "img/cl1ng.png";
+    document.getElementById("cloud6").src = "img/cl2ng.png";
+    document.getElementById("cloud7").src = "img/cl3ng.png";
+    document.getElementById("cloud8").src = "img/l4ng.png";
+    document.getElementById("cloud9").src = "img/l4ng.png";
     // document.getElementById("wave1").style.background = "url(img/waveng.png)";
     document.getElementById("wave1").setAttribute("class", "waveng");
     document.getElementById("wave2").setAttribute("class", "waveng");
@@ -67,5 +78,59 @@ function funk1(x) {
 
 
 function funk2() {
-
+    sun.setAttribute("style", "display:block ;");
+    star.setAttribute("style", "display:none ;");
+    moon.setAttribute("style", "display: none;");
+    document.getElementById("mount").src = "img/mount2.png";
+    document.getElementById("mount2").src = "img/mount21.png";
+    document.getElementById("mount3").src = "img/m1.png";
+    document.getElementById("home").src = "img/home11.png";
+    document.getElementById("bill").src = "img/billboard.png";
+    document.getElementById("tre1").src = "img/tre1.png";
+    document.getElementById("tre2").src = "img/tre2.png";
+    document.getElementById("tre3").src = "img/tre3.png";
+    document.getElementById("tre30").src = "img/tre3.png";
+    document.getElementById("tre31").src = "img/tre3.png";
+    document.getElementById("tre4").src = "img/tre3.png";
+    document.getElementById("tre41").src = "img/tre3.png";
+    document.getElementById("tre5").src = "img/tre3.png";
+    document.getElementById("tre51").src = "img/tre3.png";
+    document.getElementById("tre52").src = "img/tre3.png";
+    document.getElementById("tre53").src = "img/tre3.png";
+    document.getElementById("tre6").src = "img/tre3.png";
+    document.getElementById("tre7").src = "img/tre3.png";
+    document.getElementById("tre8").src = "img/tre3.png";
+    document.getElementById("tre9").src = "img/tre3.png";
+    document.getElementById("trees").src = "img/trees.png";
+    document.getElementById("trees2").src = "img/trees.png";
+    document.getElementById("trees3").src = "img/trees.png";
+    document.getElementById("trees4").src = "img/trees.png";
+    document.getElementById("trees41").src = "img/trees2.png";
+    document.getElementById("trees42").src = "img/trees.png";
+    document.getElementById("trees421").src = "img/trees.png";
+    document.getElementById("trees422").src = "img/trees2.png";
+    document.getElementById("trees423").src = "img/trees.png";
+    document.getElementById("trees5").src = "img/trees.png";
+    document.getElementById("trees6").src = "img/trees.png";
+    document.getElementById("trees7").src = "img/treesng.png";
+    document.getElementById("trees8").src = "img/trees2.png";
+    document.getElementById("otlar").src = "img/otlar.png";
+    document.getElementById("phon").src = "img/phone2.png";
+    // document.getElementById("wave1").style.background = "url(img/waveng.png)";
+    document.getElementById("wave1").setAttribute("class", "wave");
+    document.getElementById("wave2").setAttribute("class", "wanon");
+    document.getElementById("wave3").setAttribute("class", "wave");
+    document.getElementById("wave4").setAttribute("class", "wave");
+    document.getElementById("light12").setAttribute("style", "display: none;");
+    document.getElementById("light123").setAttribute("style", "display: none;");
+    document.getElementById("light22").setAttribute("style", "display: none;");
+    document.getElementById("light223").setAttribute("style", "display: none;");
+    document.getElementById("light32").setAttribute("style", "display: none;");
+    document.getElementById("light323").setAttribute("style", "display: none;");
+    document.getElementById("light42").setAttribute("style", "display: none;");
+    document.getElementById("light423").setAttribute("style", "display: none;");
+    document.getElementById("light52").setAttribute("style", "display: none;");
+    document.getElementById("light523").setAttribute("style", "display: block;");
+    document.getElementById("light62").setAttribute("style", "display: none;");
+    document.getElementById("light623").setAttribute("style", "display: none;");
 };
