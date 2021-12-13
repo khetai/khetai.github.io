e914fb26a20f7de3b705c4307949c4b5adbe77fe
khetai <78977971+khetai@users.noreply.github.com>
2021-12-14 01:06:01 +0400

    update header sticky on scroll

--- menu.js
+++ menu.js
@@ -9,4 +9,8 @@ mobilemenu.addEventListener("click", () => {
         menu.setAttribute('data-visible', false);
         mobilemenu.setAttribute('aria-expanded', false);
     }
+});
+window.addEventListener("scroll", function() {
+    var header = document.querySelector("header");
+    header.classList.toggle("Sticky", window.scrollY > 0);
 })
